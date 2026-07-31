53e166e475b36d831ed2b53b22aba9a5905253ee
Le Tuan Dat <147417920+Vuighe22@users.noreply.github.com>
2026-07-31 09:41:11 +0700

    update config

--- Lotus/src/main/java/com/Wiinvent/Lotus/LotusApplication.java
+++ Lotus/src/main/java/com/Wiinvent/Lotus/LotusApplication.java
@@ -1,9 +1,12 @@
 package com.Wiinvent.Lotus;
 
+import com.Wiinvent.Lotus.core.config.LotusProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(LotusProperties.class)
 public class LotusApplication {
 
 	public static void main(String[] args) {
